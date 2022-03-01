db518f00f55c75faf3bd7ffe3839261ed5af58c6
Tochi-Nwachukwu <tochinwachukwu33@gmail.com>
2022-03-01 07:41:10 +0100

    updates

--- cwg/src/Components/TimeComponent.jsx
+++ cwg/src/Components/TimeComponent.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 function TimeComponent() {
   return (
     <div>
-        
+        <div className='bg-red-300 p-6'>01</div>
+        <div>
+            <div>10:00 - 10:05 AM</div>
+            <div>Welcome and Introduction  </div>
+        </div>
     </div>
   )
 }
